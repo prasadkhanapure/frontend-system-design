2462a6e4d7a0b5959e8702af0da9b1062cffeea4
Prasad Khanapure <pnkhanapure@gmail.com>
2026-04-13 20:18:24 +0530

    CRUD using REST API (node -express JS)

--- Networking/RestApi/index.js
+++ Networking/RestApi/index.js
@@ -1,79 +1,106 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.all('/', (req, res) => {
-    // console.log('Request > ', req);
-    // console.log('Response >', res);
-    res.send(`I'm up!`);
+app.all("/", (req, res) => {
+  res.send(`I'm up!`);
 });
 
-const todos = [{
-    id: '1',
-    title: 'Task 1',
-    completed: false
-}, {
-    id: '2',
-    title: 'Task 2',
-    completed: true
-}];
+const todos = [
+  {
+    id: 1,
+    title: "Task 1",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Task 2",
+    completed: true,
+  },
+];
 
 // READ
-app.get('/todos', (req, res) => {
-    res.json(todos);
-})
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
 
 // CREATE
-app.post('/todos', (req, res) => {
-    const newTodo = req.body;
+app.post("/todos", (req, res) => {
+  const newTodo = req.body;
+  if (!newTodo.id) {
+    return res.status(401).json({
+      message: "Id is required field",
+    });
+  }
+
+  const todoIndex = todos.findIndex(
+    (td) => Number(td.id) === Number(newTodo.id),
+  );
+
+  if (todoIndex === -1) {
     todos.push(newTodo);
     res.status(201).json({
-        message: 'New Todo Added!'
+      message: "New Todo created!",
     });
+  } else {
+    res.status(409).json({
+      message: `Todo list with ${newTodo.id} is already existed.`,
+    });
+  }
 });
 
 // UPDATE
-app.put('/todos/:id', (req, res) => {
-    const newTodoData = req.body;
-    const todoParamId = req.params.id;
-    const todoIndex = todos.findIndex(td => td.id === todoParamId);
+app.put("/todos/:id", (req, res) => {
+  const newTodo = req.body;
+  const todoParamsId = req.params.id;
+  const todoIndex = todos.findIndex(
+    (item) => Number(item.id) === Number(todoParamsId),
+  );
 
-    if (todoIndex !== -1) {
-        todos[todoIndex] = {
-            id: todoParamId,
-            ...newTodoData,
-        }
-        res.json({
-            message: 'Todo updated successfully!'
-        });
-    } else {
-        res.status(400).json({
-            message: 'Todo Id does not exist!'
-        });
-    }
-    
-})
-
-// DELETE
-app.delete('/todos/:id', (req, res) => {
-    const todoParamId = req.params.id;
-    const todoIndex = todos.findIndex(td => td.id === todoParamId);
-
-    if (todoIndex !== -1) {
-        todos.splice(todoIndex, 1);
-    }
-
+  if (todoIndex !== -1) {
+    todos[todoIndex] = {
+      id: todoParamsId,
+      ...newTodo,
+    };
     res.json({
-        message: 'Todo deleted successfully!'
+      message: `Todo at id: ${todoParamsId} updated successfully!`,
     });
+  } else {
+    res.status(400).json({
+      message: "No item found!",
+    });
+  }
+});
 
-})
+// DELETE Single ID from Todo list
+app.delete("/todos/:id", (req, res) => {
+  const todoParamId = req.params.id;
+  const todoIndex = todos.findIndex(
+    (td) => Number(td.id) === Number(todoParamId),
+  );
 
+  if (todoIndex !== -1) {
+    todos.splice(todoIndex, 1);
+  }
 
-const PORT = 5111;
+  res.json({
+    message: "Todo deleted successfully!",
+  });
+});
+
+// DELETE ALL todos list
+app.delete("/todos", (req, res) => {
+  todos.length = 0;
+  return res.status(200).json({
+    message: "All todos deleted",
+    todos,
+  });
+});
+
+const PORT = 5113;
 app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`);
+  console.log(`Server is running at port ${PORT}`);
 });
